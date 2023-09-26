efc894009177a4878e1075f262686dca5a535c12
Angel Andrade <angel.bruno.andrade.osorio@gmail.com>
2023-09-26 02:34:10 -0230

    Add files via upload

---
+++ iec104_control_frames.py
@@ -0,0 +1,33 @@
+# Tramas IEC 60870-5-104 en forma de bytes
+
+# STARTDT Act (Iniciar comunicación)
+startdt_act = b'\x68\x04\x07\x00\x00\x00'
+
+# STARTDT Con (Confirmación de inicio de comunicación)
+startdt_con = b'\x68\x04\x0B\x00\x00\x00'
+
+# STOPDT Act (Detener comunicación)
+stopdt_act = b'\x68\x04\x13\x00\x00\x00'
+
+# STOPDT Con (Confirmación de detención de comunicación)
+stopdt_con = b'\x68\x04\x23\x00\x00\x00'
+
+# TESTFR Act (Prueba de comunicación)
+testfr_act = b'\x68\x04\x43\x00\x00\x00'
+
+# TESTFR Con (Confirmación de prueba de comunicación)
+testfr_con = b'\x68\x04\x83\x00\x00\x00'
+
+# ACK (Acknowledge)
+ack = b'\x68\x04\x0D\x00\x00\x00'
+
+# NACK (Not Acknowledge)
+nack = b'\x68\x04\x0E\x00\x00\x00'
+
+# Solicitud de Enlace Activo (ACT)
+solicitud_act = b'\x68\x04\x09\x00\x00\x00'
+
+# Confirmación de Enlace Activo (ACT CON)
+confirmacion_act_con = b'\x68\x04\x0F\x00\x00\x00'
+
+# Puedes usar estas tramas según tus necesidades en tu aplicación IEC 60870-5-104.
