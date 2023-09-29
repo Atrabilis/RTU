33650e151798eaa3cfc97ecb78d3e722b5e75611
angel <angel.bruno.andrade.osorio@gmail.com>
2023-09-29 05:23:27 -0300

    Milestone 1 - mantener conexion y recibir datos

--- client.py
+++ client.py
@@ -7,17 +7,19 @@ SERVER_IP = 'localhost'  # Change this to the RTU's IP address
 SERVER_PORT = 2404
 
 # Configure the T3 time for the interrogation
-INTERROGATION_INTERVAL = 10  # 60 seconds (adjust as needed)
+INTERROGATION_INTERVAL = 1  # 60 seconds (adjust as needed)
 
-def send_interrogation_apdu(client_socket):
-    # Define the Interrogation General APDU
-    interrogation_apdu = b'\x68\x0e\x14\x00\x34\x00\x64\x01\x06\x00\xff\xff\x00\x00\x00\x14'  # Adjust this according to your specific requirements
-    
+def send_interrogation_apdu(client_socket, increment):
+    # Incrementar el quinto byte en el APDU
+    interrogation_apdu = bytearray(b'\x68\x04\x01\x00\x00\x00')
+    interrogation_apdu[4] = (interrogation_apdu[4] + increment) & 0xFF  # Incrementa el quinto byte
+
     # Send the Interrogation General APDU
     client_socket.send(interrogation_apdu)
 
 def main():
     start_time = time.time()  # Record the start time
+    incremento = 0  # Inicializar el incremento en 0
 
     # Create a socket for the client
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as client_socket:
@@ -28,14 +30,17 @@ def main():
             print(f"Connected to {SERVER_IP}:{SERVER_PORT}")
             time.sleep(1)
             while True:
-                # Send the Interrogation General APDU
-                send_interrogation_apdu(client_socket)
+                # Send the Interrogation General APDU with the current incremento
+                send_interrogation_apdu(client_socket, incremento)
 
                 # Receive data from the RTU
                 received_data = client_socket.recv(1024)  # Adjust the buffer size as needed
 
                 # Process or print the received data as needed
                 print(f"Received data: {received_data}")  # Assuming UTF-8 encoding
+
+                # Incrementar el valor del incremento
+                incremento += 2
 
                 # Sleep for the interrogation interval before sending the next APDU
                 time.sleep(INTERROGATION_INTERVAL)
